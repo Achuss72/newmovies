017a4de2e57ac1075f8082e7f9b700b0bb5959b0
Proffser <89152021+Proffser@users.noreply.github.com>
2022-07-04 21:19:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
 git clone -b master https://github.com/Proffser/ngc_muthmani
-cd /ngc_muthmani
+cd/ngc_muthmani
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
